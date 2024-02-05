9cd62fc4c60b4c498b75088281efb5c45554b805
DGooden Gooden <infinityshold0227@gmail.com>
2024-02-05 16:50:44 +0000

    New way of submitting! and data shapes end

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -61,13 +61,24 @@ $(document).ready(function () {
   
 
   // TODO 3-a: add a function that handles the static display type
-  
+  function handleStatic(data) {
+    setBackgroundWithObject(data);
+    animationDetails.displayType = 1;
+  }
 
   // TODO 4-a: add a function that handles the good display type
-  
+  function handleGood(color, shape, repeat){
+    setBackgroundWithSimple(color, shape, repeat);
+    animationDetails.displayType = 2;
+
+  }
 
   // TODO 5-a: add a function that handles the bad display type
-  
+  function handleBad(data, repeat) {
+    repeat += 1;
+    setBackgroundWithMixed(data, repeat)
+    animationDetails.displayType = 3;
+  }
 
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
@@ -75,17 +86,20 @@ $(document).ready(function () {
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
-    
+    handleStatic(dataShapes[currentIndex])
   }
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    
+    var currentShape = dataShapes[currentIndex]; 
+    handleGood(currentShape.color, currentShape.shape, currentShape.repeat)
   }
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    
+    var currentShape = dataShapes[currentIndex];
+    var repeat = currentShape.repeat
+    handleBad(currentShape, repeat)
   }
 
   /////////////////////////////////////////////////
@@ -101,7 +115,7 @@ $(document).ready(function () {
     const data = [];
     const colors = ["red", "green", "blue"];
     const shapes = ["square", "triangle", "circle"];
-    const repeats = [1, 2, 3,];
+    const repeats = [1, 2, 3, 4, 5];
 
     for (var i = 0; i < colors.length; i++) {
       for (var j = 0; j < shapes.length; j++) {
